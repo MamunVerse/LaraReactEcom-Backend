65347c677ddea0bb5643c23c668c471e9becff3d
MamunVerse <mamunurrashid6424@gmail.com>
2022-09-14 03:06:30 +0600

    Make login route & Login controller

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -39,4 +39,36 @@ class AuthController extends Controller
             ]);
         }
     }
+
+    public function login(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            "email" => 'required|email',
+            "password" => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                "validation_error" => $validator->messages(),
+            ]);
+        }else{
+            $user = User::where('email', $request->email)->first();
+
+            if(!$user || ! Hash::check($request->password, $user->password)){
+                return response()->json([
+                    "status" => 401,
+                    "message" => "Invalid Credentials"
+                ]);
+            }else{
+
+                $token = $user->createToken($user->email.'_token')->plainTextToken;
+                return response()->json([
+                    "status" => 200,
+                    "username" => $user->name,
+                    "token" => $token,
+                    "message" => "Login Successfully",
+                ]);
+            }
+        }
+    }
 }
