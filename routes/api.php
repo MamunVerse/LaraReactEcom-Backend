a539c26a697851345d17e9b0fcfd57c50567cced
MamunVerse <mamunurrashid6424@gmail.com>
2022-09-15 02:16:23 +0600

    Fix normal user logout issue

--- routes/api.php
+++ routes/api.php
@@ -23,14 +23,17 @@ Route::middleware(['auth:sanctum', 'isAPIAdmin'])->group(function () {
     Route::get('/checkingAuthenticated', function(){
         return response()->json(["message"=>"You are in", "status" => 200]);
     });
+
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
-
 });
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 
 
